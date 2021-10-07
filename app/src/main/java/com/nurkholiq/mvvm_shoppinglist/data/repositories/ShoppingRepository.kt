472ba5c185bab8f6bf7e2684501c271583e3ac5a
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-07 10:25:10 +0700

    Add new file class ShoppingRepository.kt

---
+++ app/src/main/java/com/nurkholiq/mvvm_shoppinglist/data/repositories/ShoppingRepository.kt
@@ -0,0 +1,14 @@
+package com.nurkholiq.mvvm_shoppinglist.data.repositories
+
+import com.nurkholiq.mvvm_shoppinglist.data.db.ShoppingDatabase
+import com.nurkholiq.mvvm_shoppinglist.data.db.entities.ShoppingItem
+
+class ShoppingRepository(
+    private val db: ShoppingDatabase
+) {
+    suspend fun upsesrt(item: ShoppingItem) = db.getShoppingDao().upsert(item)
+
+    suspend fun delete(item: ShoppingItem) = db.getShoppingDao().delete(item)
+
+    fun getAllShoppingItem() = db.getShoppingDao().getAllShoppingItems()
+}
