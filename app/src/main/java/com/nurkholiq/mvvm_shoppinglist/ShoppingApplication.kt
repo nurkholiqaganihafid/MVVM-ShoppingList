8c13bde29847f3fbec99dd95e0b2ac25866761fe
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-08 15:42:59 +0700

    added new class file ShoppingApplication.kt for dependency injection with codeine because objects in activity class are very bad

---
+++ app/src/main/java/com/nurkholiq/mvvm_shoppinglist/ShoppingApplication.kt
@@ -0,0 +1,25 @@
+package com.nurkholiq.mvvm_shoppinglist
+
+import android.app.Application
+import com.nurkholiq.mvvm_shoppinglist.data.db.ShoppingDatabase
+import com.nurkholiq.mvvm_shoppinglist.data.repositories.ShoppingRepository
+import com.nurkholiq.mvvm_shoppinglist.ui.shoppinglist.ShoppingViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class ShoppingApplication : Application(), KodeinAware {
+
+    override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@ShoppingApplication))
+        bind() from singleton { ShoppingDatabase(instance()) }
+        bind() from singleton { ShoppingRepository(instance()) }
+        bind() from provider {
+            ShoppingViewModelFactory(instance())
+        }
+    }
+}
