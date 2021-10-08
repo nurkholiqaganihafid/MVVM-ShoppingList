6afa8d04fefa2bf8e5b96ed4de7a9c3c90061397
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-08 13:58:06 +0700

    modified ShoppingActivity.kt dengan menerapkan recyclerview dan menampilkan semua item dalam database di tampilan recyclerview

--- app/src/main/java/com/nurkholiq/mvvm_shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/nurkholiq/mvvm_shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,10 +2,14 @@ package com.nurkholiq.mvvm_shoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nurkholiq.mvvm_shoppinglist.R
 import com.nurkholiq.mvvm_shoppinglist.data.db.ShoppingDatabase
 import com.nurkholiq.mvvm_shoppinglist.data.repositories.ShoppingRepository
+import com.nurkholiq.mvvm_shoppinglist.other.ShoppingItemAdapter
+import kotlinx.android.synthetic.main.activity_shopping.*
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,16 @@ class ShoppingActivity : AppCompatActivity() {
         val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemAdapter(listOf(),viewModel)
+
+        rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        rvShoppingItems.adapter = adapter
+
+        viewModel.getAllShoppingItem().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
 
     }
 }
